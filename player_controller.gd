75e54277b8e915281e4b09a5b9d7b0d0fcfc3cfb
Garrett Gunnell <gunnellg@oregonstate.edu>
2025-04-11 22:38:05 -0700

    World boundaries

--- player_controller.gd
+++ player_controller.gd
@@ -1,13 +1,20 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+const SPEED = 10.0
+
+func _ready() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	var camera = $Camera3D
+	
+	var direction = (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -16,3 +23,9 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			get_tree().quit()
